c974c5c9c8aff634286478f9e80ba099d42c1383
AleksandrEgorov <ferociousrat@mail.ru>
2022-11-26 18:45:33 +0300

    dz aiohttp 2

--- aio.py
+++ aio.py
@@ -21,6 +21,16 @@ class Notifications(Base):
     owner = Column(String, nullable=False)
 
 
+async def app_context(app: web.Application):
+    async with engine.begin() as dat_base_connect:
+        async with Session() as session:
+            await session.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+            await session.commit()
+        await dat_base_connect.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
 async def rat_page(request):
     return web.json_response({'rat': 'rat'})
 
@@ -28,53 +38,46 @@ async def rat_page(request):
 class Notis(web.View):
 
     async def get(self):
-
-        async with engine.begin() as dat_base_connect:
-            await dat_base_connect.run_sync(Base.metadata.create_all)
-            await dat_base_connect.commit()
-
-        Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
         async with Session() as session:
             noti_id = self.request.match_info['noti_id']
-            responce = await session.get(Notifications(noti_id=noti_id), int(noti_id))
-        return web.json_response({'sucsess': responce})
+            responce = await session.get(Notifications, int(noti_id))
+        return web.json_response({'success': {'owner': responce.owner,
+                                              'header': responce.header,
+                                              'description': responce.description,
+                                              'creation_date': str(responce.creation_date)
+                                              }
+                                  })
 
     async def post(self):
-
-        async with engine.begin() as dat_base_connect:
-            await dat_base_connect.run_sync(Base.metadata.create_all)
-            await dat_base_connect.commit()
-
-        Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
         data = await self.request.json()
         async with Session() as session:
             session.add(Notifications(header=data['header'], description=data['description'], owner=data['owner']))
             await session.commit()
-
-        return web.json_response({'sucsses': 'note added'})
+        return web.json_response({'success': 'note added'})
 
     async def patch(self):
-        async with engine.begin() as dat_base_connect:
-            await dat_base_connect.run_sync(Base.metadata.create_all)
-            await dat_base_connect.commit()
-
-        Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-        noti_id = self.request.match_info['noti_id']
-        data = await self.request.json()
         async with Session() as session:
-            session.patch(Notifications(header=data['header'], description=data['description'], owner=data['owner']))
+            noti_id = self.request.match_info['noti_id']
+            data = await self.request.json()
+            obj = await session.get(Notifications, int(noti_id))
+            for x, y in data.items():
+                setattr(obj, x, y)
             await session.commit()
-
-        return web.json_response({'sucsses': 'note pathed'})
+        return web.json_response({'success': 'note pathed'})
 
     async def delete(self):
-        return web.json_response()
+        async with Session() as session:
+            noti_id = self.request.match_info['noti_id']
+            print(noti_id)
+            obj = await session.get(Notifications, int(noti_id))
+            print(obj)
+            await session.delete(obj)
+            await session.commit()
+        return web.json_response({'success': 'deleted'})
 
 
 app = web.Application()
+app.cleanup_ctx.append(app_context)
 app.router.add_get('', rat_page)
 app.router.add_post('/notifications', Notis)
 app.router.add_get('/notifications/{noti_id:\d+}', Notis)
